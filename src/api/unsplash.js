f221fb46239ccc802bf01b16ceed0d5710c38238
Faisal-Alqabbani <51875054+Faisal-Alqabbani@users.noreply.github.com>
2020-09-27 01:41:13 +0000

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID Qqj2ma-fSV8rEJ7SJotWXFIqz3tZ6PLAOK37FvFPTbg",
+    Authorization: "API_KEY",
   },
 });
